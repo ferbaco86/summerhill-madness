bb35e577cad1b403fd09b6c3c2ac5858a092187d
Fernando Bahamondes <ferbaco@gmail.com>
2020-11-08 13:50:31 -0300

    Update options scene

--- src/scenes/optionsScene.js
+++ src/scenes/optionsScene.js
@@ -5,9 +5,52 @@ export default class OptionsScene extends Phaser.Scene {
     super('Options');
   }
 
-  preload() {
-  }
 
   create() {
+    this.musicOn = true;
+    this.soundOn = true;
+
+    this.text = this.add.text(300, 100, 'Options', { font: '62px ThaleahFat' });
+    this.musicButton = this.add.image(200, 200, 'checkedBox');
+    this.musicText = this.add.text(250, 190, 'Music Enabled', { font: '42px ThaleahFat' });
+
+    this.soundButton = this.add.image(200, 300, 'checkedBox');
+    this.soundText = this.add.text(250, 290, 'Sound Enabled', { font: '42px ThaleahFat' });
+
+    this.musicButton.setInteractive();
+    this.soundButton.setInteractive();
+
+    this.musicButton.on('pointerdown', () => {
+      this.musicOn = !this.musicOn;
+      this.updateAudio();
+    });
+
+    this.soundButton.on('pointerdown', () => {
+      this.soundOn = !this.soundOn;
+      this.updateAudio();
+    });
+
+    this.updateAudio();
+    this.menuButton = this.add.sprite(400, 500, 'blueButton1').setInteractive();
+    this.menuText = this.add.text(0, 0, 'Menu', { font: '42px ThaleahFat', fill: '#fff' });
+    Phaser.Display.Align.In.Center(this.menuText, this.menuButton);
+
+    this.menuButton.on('pointerdown', () => {
+      this.scene.start('Title');
+    });
+  }
+
+  updateAudio() {
+    if (this.musicOn === false) {
+      this.musicButton.setTexture('box');
+    } else {
+      this.musicButton.setTexture('checkedBox');
+    }
+
+    if (this.soundOn === false) {
+      this.soundButton.setTexture('box');
+    } else {
+      this.soundButton.setTexture('checkedBox');
+    }
   }
 }
