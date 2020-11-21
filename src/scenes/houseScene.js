e8883bdae28533a920691f85d10edbf026e55297
Fernando Bahamondes <ferbaco@gmail.com>
2020-11-21 03:31:01 -0300

    Refactor House Scene movement with module

--- src/scenes/houseScene.js
+++ src/scenes/houseScene.js
@@ -1,5 +1,7 @@
 import Phaser from 'phaser';
 import generateMaps from '../utils/generateMaps';
+import characterMov from '../utils/characterMovement';
+import mainCharAnimInfo from '../assets/data/mainCharAnims.json';
 
 export default class HouseScene extends Phaser.Scene {
   constructor() {
@@ -41,42 +43,7 @@ export default class HouseScene extends Phaser.Scene {
   }
 
   update() {
-    const speed = 155;
-    const prevVelocity = this.mainChar.body.velocity.clone();
-    this.mainChar.body.setVelocity(0);
-
-    // Horizontal movement
-    if (this.cursors.left.isDown) {
-      this.mainChar.body.setVelocityX(-60);
-    } else if (this.cursors.right.isDown) {
-      this.mainChar.body.setVelocityX(60);
-    }
-
-    // Vertical movement
-    if (this.cursors.up.isDown) {
-      this.mainChar.body.setVelocityY(-60);
-    } else if (this.cursors.down.isDown) {
-      this.mainChar.body.setVelocityY(60);
-    }
-
-    this.mainChar.body.velocity.normalize().scale(speed);
-
-    if (this.cursors.left.isDown) {
-      this.mainChar.anims.play('mainCharWalkLeft', true);
-    } else if (this.cursors.right.isDown) {
-      this.mainChar.anims.play('mainCharWalkRight', true);
-    } else if (this.cursors.up.isDown) {
-      this.mainChar.anims.play('mainCharWalkUp', true);
-    } else if (this.cursors.down.isDown) {
-      this.mainChar.anims.play('mainCharWalkDown', true);
-    } else {
-      this.mainChar.anims.stop();
-
-      // If we were moving, pick and idle frame to use
-      if (prevVelocity.x < 0) this.mainChar.setTexture('mainLeft', 1);
-      else if (prevVelocity.x > 0) this.mainChar.setTexture('mainRight', 1);
-      else if (prevVelocity.y < 0) this.mainChar.setTexture('mainUp', 1);
-      else if (prevVelocity.y > 0) this.mainChar.setTexture('mainDown', 1);
-    }
+    characterMov.charMovementControl(this.mainChar, this.cursors, 155, 50, -50, -50, 50,
+      mainCharAnimInfo, 1);
   }
 }
