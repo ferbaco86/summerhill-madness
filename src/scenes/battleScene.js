99881381a078195f75430474e5915ff0545bc213
Fernando Bahamondes <ferbaco@gmail.com>
2020-11-24 19:10:17 -0300

    Add enemy battler to scene

--- src/scenes/battleScene.js
+++ src/scenes/battleScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import Enemy from '../objects/enemy';
 import Player from '../objects/player';
 
 
@@ -13,6 +14,7 @@ export default class BattleScene extends Phaser.Scene {
     this.add.image(0, -200, 'townBattleBG').setOrigin(0, 0).setScale(2);
     const mainChar = new Player(this, 700, 200, 'mainCharBattleStand', 1, 'Main', 100, 20);
     const redHead = new Player(this, 750, 330, 'redHeadBattleStand', 1, 'Ro', 100, 8);
+    const blueSlime = new Enemy(this, 100, 200, 'blueSlimeBattler', 0, 'slime', 50, 15);
   }
 
   update() {
